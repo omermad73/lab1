087dbd3b8d104cfbe2ad4e482977f213013790b1
DoctorB <150272448+DoctorB-CA@users.noreply.github.com>
2024-05-17 16:58:49 +0300

    Add files via upload

---
+++ GNO - Generic Networking Object.py
@@ -0,0 +1,7 @@
+class GNO: #Generic Networking Object
+    Count_Objects = 0
+
+    def __init__(self, type):
+        self.id = GNO.Count_Objects
+        self.type = type
+        GNO.Count_Objects =+1
