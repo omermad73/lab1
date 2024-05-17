919c0ab6cb60aef2cbb871c9ac3c94e6c7e926bd
DoctorB <150272448+DoctorB-CA@users.noreply.github.com>
2024-05-17 17:43:46 +0300

    Update Host.py

--- Host.py
+++ Host.py
@@ -1,56 +1,48 @@
 import random
+from GNO import GNO
+from Event import Event
 
-class Event:
-    def __init__(self, time, event_type, sender_id, receiver_id, message_id):
-        self.time = time
-        self.event_type = event_type
-        self.sender_id = sender_id
-        self.receiver_id = receiver_id
-        self.message_id = message_id
 
-class L2Message:
-    def __init__(self, l2_message_id, source_mac, destination_mac, payload_size, data):
-        self.l2_message_id = l2_message_id
-        self.source_mac = source_mac
-        self.destination_mac = destination_mac
-        self.payload_size = payload_size
-        self.data = data
 
-class Host:
-    def __init__(self, host_id, mac, nic):
-        self.time = PartA.startTime
-        self.host_id = host_id
+class L2Message(GNO):
+    def __init__(self, mac, nic, total_tx_bytes, total_rx_bytes):
+        super().__init__("Host")
         self.mac = mac
         self.nic = nic
-        self.total_tx_bytes = 0
-        self.total_rx_bytes = 0
-
-    def get_host_id(self):
-        return self.host_id
-
-    def get_mac(self):
-        return self.mac
-
-    def set_mac(self, mac):
-        self.mac = mac
-
-    def get_nic(self):
-        return self.nic
-
-    def set_nic(self, nic):
-        self.nic = nic
-
-    def get_total_tx_bytes(self):
-        return self.total_tx_bytes
-
-    def set_total_tx_bytes(self, total_tx_bytes):
         self.total_tx_bytes = total_tx_bytes
-
-    def get_total_rx_bytes(self):
-        return self.total_rx_bytes
-
-    def set_total_rx_bytes(self, total_rx_bytes):
         self.total_rx_bytes = total_rx_bytes
+
+    @property
+    def mac(self):
+        return self._mac
+
+    @mac.setter
+    def mac(self, value):
+        self._mac = value
+
+    @property
+    def nic(self):
+        return self._nic
+
+    @nic.setter
+    def nic(self, value):
+        self._nic = value
+
+    @property
+    def total_tx_bytes(self):
+        return self._total_tx_bytes
+
+    @total_tx_bytes.setter
+    def total_tx_bytes(self, value):
+        self._total_tx_bytes = value
+
+    @property
+    def total_rx_bytes(self):
+        return self._total_rx_bytes
+
+    @total_rx_bytes.setter
+    def total_rx_bytes(self, value):
+        self._total_rx_bytes = value
 
     def create_l2_message(self, l2_message_id, printing_flag, all_hosts, min_payload_size, max_payload_size):
         # New "create a message" event
@@ -70,7 +62,8 @@ class Host:
 
     def receive_l2_message(self, printing_flag, l2_message):
         # New "receive a message" event
-        event = Event(time.time() - self.time, "receive a message", self.host_id, self.host_id, l2_message.l2_message_id)
+        event = Event(time.time() - self.time, "receive a message", self.host_id, self.host_id,
+                      l2_message.l2_message_id)
 
         # Update the total bytes received
         self.total_rx_bytes += l2_message.payload_size
@@ -100,5 +93,5 @@ class Host:
     def get_random_payload_size(self, min_payload_size, max_payload_size):
         # Generate a random payload size between the min and max payload size
         payload_size = random.randint(min_payload_size, max_payload_size)
-        
+
         return payload_size
