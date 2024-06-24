7f67831113dbb41f13d1e92cd7415f178e629528
DoctorB <150272448+DoctorB-CA@users.noreply.github.com>
2024-06-24 13:32:13 +0300

    Update Switch2.py

--- Switch2.py
+++ Switch2.py
@@ -17,9 +17,15 @@ class SwitchLab2(Switch):
         for i in range(num_ports):
             self.queue_to_HoLTime[i] = 0
         self.port_is_blocked = [False] * self.num_ports
-        self.flooding_tabel = [[],[],[],[]]
+        self.flooding_tabel = self.configure_flooding_tabel()
         self.start_to_flood = False
 
+    def configure_flooding_tabel(self):
+        port_is_blocked = []
+        for i in range(0,self.num_ports):
+            port_is_blocked.append([])
+        return port_is_blocked
+    
     def configure_queues(self):
         if self.q_type == 'input' or self.q_type == 'output':
             return [queue.Queue() for _ in range(self.num_ports)]
@@ -92,12 +98,15 @@ class SwitchLab2(Switch):
 
         time = timeline.events[0].scheduled_time
         dest_port = self.find_port(dst_mac, current_time)
-
-        if self.queues[port].empty():
+        if dest_port == None:
+            self.sending_float(l2_message, all_l2messages, timeline, current_time, link_id, printing_flag)
+        else:
             self.sending_non_float(l2_message, all_l2messages, timeline, current_time, link_id, printing_flag)
-            if not self.port_is_blocked[dest_port]:
-                event = Event(time, "sending a message", self.id, self.id, l2_message.id, self.ports[port])
-                timeline.add_event(event)
+
+        self.trans(timeline,all_l2messages,printing_flag)
+        # if not self.port_is_blocked[dest_port]:
+        #     event = Event(time, "sending a message", self.id, self.id, l2_message.id, self.ports[port])
+        #     timeline.add_event(event)
 
     def sending_non_float(self, l2_message, all_l2messages, timeline, current_time, link_id, printing_flag):
         #if can - sending non floading messaeg
@@ -121,19 +130,61 @@ class SwitchLab2(Switch):
         dst_mac = l2_message.dst_mac
         port = self.link_to_port(link_id)
         time = timeline.events[0].scheduled_time
-        self.start_to_flood = True
+
 
         if not self.start_to_flood:
-            for dest_port in self.ports:
+            self.start_to_flood = True
+            for dest_port in range(0,self.num_ports):
                 if dest_port != port:
                     self.flooding_tabel[port].append(dest_port)  # adding all the port we need to flood
-                    if not self.port_is_blocked[dest_port]:  # if the  out port is free
-                        copy_l2_message = copy.copy(l2_message)
-                        event = Event(time, "sending a message", self.id, self.id, copy_l2_message.id,
-                                      self.ports[dest_port])  # send the mesage
-                        timeline.add_event(event)
+                    #if not self.port_is_blocked[dest_port]:  # if the  out port is free
+                        # copy_l2_message = copy.copy(l2_message)
+                        # event = Event(time, "sending a message", self.id, self.id, copy_l2_message.id,
+                        #               self.ports[dest_port])  # send the mesage
+                        # timeline.add_event(event)
         return
 
+    def who_the_longest_waiting(self,dest_port):
+        # in given dest port - return the sorce port the wait the most time
+        longest_port = 0
+        for port in range(0,self.num_ports):
+            if dest_port in self.flooding_tabel[port]:
+                if self.queue_to_HoLTime[port] < self.queue_to_HoLTime[longest_port]:
+                    longest_port = port
+        return  longest_port
+
+    def trans(self,timeline,all_l2messages,printing_flag):
+        #for every free output port sedn the message that wait the most time
+        for dest_port in range(0,self.num_ports):
+            if not self.port_is_blocked[dest_port]:
+                longest_port = self.who_the_longest_waiting(dest_port)
+                self.trans_per_mess(timeline,longest_port,dest_port,all_l2messages,printing_flag)
+
+
+    def trans_per_mess(self,timeline,longest_port,dest_port,all_l2messages,printing_flag):
+        if dest_port not in self.flooding_tabel[longest_port]:
+            return
+        time = timeline.events[0].scheduled_time
+        l2_message = self.dequeue(longest_port)
+        self.port_is_blocked[longest_port] = True
+        self.flooding_tabel[longest_port].remove(dest_port)
+        self.send_message(timeline, dest_port, l2_message, all_l2messages)
+        if printing_flag == 1:
+            print(
+                f"Switch: {self.id} \033[36msending\033[0m the message (size: {l2_message.message_size}) to port {dest_port} at time: {current_time:.6f}")
+
+
+    def message_transmitted_input(self, timeline, queue_num, l2_message, all_l2messages, printing_flag):
+        current_time = timeline.events[0].scheduled_time
+        input_port = self.self.get_real_queue(queue_num)[0]
+        if self.q_type == 'virtual_output':
+            output_port = self.self.get_real_queue(queue_num)[1] # todo: are you sure
+
+        next_message = self.dequeue(input_port)
+        if next_message is not None:
+            dst_mac = next_message.dst_mac
+            # find the destination port for next message
+            self.sending_non_float(self, l2_message, all_l2messages, timeline, current_time, link_id, printing_flag)
 
     def handle_head_input(self,printing_flag):
         dst_mac = l2_message.dst_mac
@@ -264,31 +315,53 @@ class SwitchLab2(Switch):
         else:
             self.message_transmitted_output(timeline, queue_num, printing_flag)
 
-    def message_transmitted_input(self, timeline, queue_num, printing_flag):
+    def message_transmitted_output(self, timeline, queue_num, all_l2messages, printing_flag):
         current_time = timeline.events[0].scheduled_time
-        if printing_flag == 1:
-            print(f"Switch: {self.id} \033[32mtransmitted\033[0m a message to link {link.id} at time: "
-                  f"{current_time:.6f}")
 
-
+        self.port_is_blocked[queue_num] = False
+        self.totalHoLTime += current_time - self.queue_to_HoLTime[queue_num]
+        self.queue_to_HoLTime[queue_num] = 0
         next_message = self.dequeue(queue_num)
-        dst_mac = next_message.dst_mac
-        source_port = queue_num
-        dest_port = self.find_port(dst_mac, current_time)
-        if dest_port is not None:  # If the destination port is found in the MAC table
-            if self.ports[dest_port] is not None:  # If the destination port is connected
-                if source_port != dest_port:  # if not the switch should drop the message
-                    self.enqueue(next_message, dest_port)
-            else:  # that if the link was disconnected
-                pass
-        else:
-            for out_port, link in enumerate(self.ports):
-                if out_port != source_port and link is not None:
-                    duplicated_message = copy.copy(next_message)
-                    self.enqueue(duplicated_message, out_port)
-            if printing_flag == 1:
-                print(f"Switch: {self.id} \033[35m will flood\033[0m the message (size: {next_message.message_size}"
-                      f") "f"at time: {current_time:.6f}")
+        if next_message is None:
+            return
+
+        dest_port = queue_num
+        link = self.ports[queue_num]
+        time = current_time + link.transmit_delay(next_message)  # calculation of arrival time
+        # = time of sending
+        self.port_is_blocked[queue_num] = True
+        event = Event(time, "transmitted", self.id, None, None, queue_num)
+        timeline.add_event(event)
+        if printing_flag == 1:
+            print(f"Switch: {self.id} \033[36msending\033[0m the message (size: {next_message.message_size}) to"
+                  f" port {dest_port} at time: {current_time:.6f}")
+        self.send_message(timeline, dest_port, next_message, all_l2messages)
+
+    def first_message_input(self, timeline, queue_num, all_l2messages, printing_flag):
+        current_time = timeline.events[0].scheduled_time
+
+        next_message = self.queues[queue_num].get()
+        self.queues[queue_num].put(next_message)
+
+        self.port_is_blocked[queue_num] = False
+        self.totalHoLTime += self.queue_to_HoLTime[queue_num]
+        self.queue_to_HoLTime[queue_num] = 0
+        next_message = self.queues[queue_num].get()
+        self.queues[queue_num].put(next_message)
+        if next_message is None:
+            return
+
+        dest_port = queue_num
+        link = self.ports[queue_num]
+        time = current_time + link.transmit_delay(next_message)  # calculation of arrival time
+        # = time of sending
+        self.port_is_blocked[queue_num] = True
+        event = Event(time, "transmitted", self.id, None, None, queue_num)
+        timeline.add_event(event)
+        if printing_flag == 1:
+            print(f"Switch: {self.id} \033[36msending\033[0m the message (size: {next_message.message_size}) to"
+                  f" port {dest_port} at time: {current_time:.6f}")
+        self.send_message(timeline, dest_port, next_message, all_l2messages)
 
     def message_transmitted_output(self, timeline, queue_num, all_l2messages, printing_flag):
         current_time = timeline.events[0].scheduled_time
